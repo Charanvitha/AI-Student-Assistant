e924153c71d85c3a89a062416f548a7decd400b8
charanvithad <charanvithadeshini@gmail.com>
2026-07-04 13:15:56 +0530

    Debug MongoDB URI

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 export async function connectDatabase() {
-  const uri = process.env.MONGODB_URI;
-  if (!uri) {
-    throw new Error('MONGODB_URI is required');
-  }
+  console.log("URI:", process.env.MONGODB_URI);
 
-  mongoose.set('strictQuery', true);
+  const uri = process.env.MONGODB_URI;
+
+  mongoose.set("strictQuery", true);
   await mongoose.connect(uri);
-  console.log('MongoDB connected');
+
+  console.log("MongoDB connected");
 }
